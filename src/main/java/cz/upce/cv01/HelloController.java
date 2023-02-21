cda63a9027d9d8df35361cdb6fb0ac39acff0da0
Filda360 <Filda360@seznam.cz>
2023-02-21 10:09:59 +0100

    0

---
+++ src/main/java/cz/upce/cv01/HelloController.java
@@ -0,0 +1,14 @@
+package cz.upce.cv01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+    @GetMapping("")
+    public String helloWorld() {
+        return "Hello world from Spring Boot application.";
+    }
+}
