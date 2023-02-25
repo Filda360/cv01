935781f72e38f568894fbb4023c9d136e1441f8c
Filda360 <Filda360@seznam.cz>
2023-02-26 00:47:10 +0100

    0

--- src/main/java/cz/upce/cv01/HelloController.java
+++ src/main/java/cz/upce/cv01/HelloController.java
@@ -1,14 +1,47 @@
 package cz.upce.cv01;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//?1)
+//@Controller - vraci HTTP response
+//@RestController - pro REST API, vrací JSON
+
+//?2)
+// JSON
+//{
+//  "myInfo": {
+//    "firstName": "Sumeet"
+//  }
+//}
+// YAML
+//  myInfo:
+//      firstName:Jmeno
+//  XML - <myInfo><firstName>Jmeno</firstName></myInfo>
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+//@RequestMapping("/api/v1") //-> application.properties
 public class HelloController {
     @GetMapping("")
     public String helloWorld() {
         return "Hello world from Spring Boot application.";
+    }
+
+    @GetMapping("/path/{message}")
+    public String helloPath(@PathVariable String message){
+        //localhost:9000/api/v1/path/ahoj
+        return "Message from path variable: " + message;
+    }
+
+    @GetMapping("/query")
+    public String helloQuery(@RequestParam(name = "id") String message){
+        //localhost:9000/api/v1/query?id=ahoj
+        //id -> message default
+        return "Message from query variable: " + message;
+    }
+
+    @GetMapping("/body")
+    public String helloRequestBody(@RequestBody SimpleBody body){
+        //localhost:9000/api/v1/
+        return "Message from body: " + body.toString();
     }
 }
---
+++ src/main/java/cz/upce/cv01/SimpleBody.java
@@ -0,0 +1,24 @@
+package cz.upce.cv01;
+
+public class SimpleBody {
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleBody{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+    public SimpleBody(String message) {
+        this.message = message;
+    }
+
+    private String message;
+}
